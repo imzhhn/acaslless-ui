5a9f2c01527529718fd919261ac3f30a094600d9
张颢瀚 <1085155177@qq.com>
2026-04-27 08:58:01 +0800

    feat: 项目初始化（VUE、ELECTRON）

---
+++ electron/main.js
@@ -0,0 +1,36 @@
+const { app, BrowserWindow } = require('electron')
+const path = require('path')
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1200,
+    height: 800,
+    webPreferences: {
+      nodeIntegration: false,
+      contextIsolation: true
+    }
+  })
+
+  // 开发模式下加载 localhost
+  if (process.env.NODE_ENV === 'development') {
+    win.loadURL('http://localhost:8080')
+    win.webContents.openDevTools()
+  } else {
+    // 生产模式下加载打包后的文件
+    win.loadFile(path.join(__dirname, 'dist', 'index.html'))
+  }
+}
+
+app.whenReady().then(createWindow)
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow()
+  }
+})
